e1ecf88dace504ad35d489c797793217cdb4e2f6
ariadiag <ariadiag@users.noreply.github.com>
2020-02-20 23:37:07 -0500

    working on frogger game

---
+++ Assets/Frogger/Frog.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+	public Rigidbody2D rb;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+			rb.MovePosition(rb.position + Vector2.right);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
+			rb.MovePosition(rb.position - Vector2.right);
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)){
+			rb.MovePosition(rb.position + Vector2.up);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)){
+			rb.MovePosition(rb.position - Vector2.up);
+		}
+    }
+}

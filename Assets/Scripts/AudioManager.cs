106b22f49806495adf226e2c89499a418c6dd246
Morgan <63208616+Total-Farmage@users.noreply.github.com>
2020-04-22 17:25:35 -0400

    AudioManager and SFX
    rescripted an AudioManager that /should/ work, as well as creating a folder for our SFX.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sfx;
+    
+    // Use this for things needing initialization
+    void Awake()
+    {
+        foreach (Sound curr in sfx)
+        {
+            curr.source = gameObject.AddComponent<AudioSource>();
+            curr.source.clip = curr.clip;
+            curr.source.loop = curr.loop;
+
+            curr.source.volume = curr.volume;
+            curr.source.pitch = curr.pitch;
+        }
+    }
+
+    // Use this method to actually play the sound you're looking for.
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sfx, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound '" + name + "' isn't real, dude.");
+            return;
+        }
+        s.source.Play();
+    }
+}

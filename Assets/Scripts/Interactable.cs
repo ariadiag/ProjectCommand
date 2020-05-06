c6e15bce375b0cf43028bd9ea7587f77bb73dc7b
ariadiag <ariadiag@users.noreply.github.com>
2020-05-05 21:43:26 -0400

    note can now be dropped!

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Interactable : MonoBehaviour
 {
     public Player player;
 	public GameObject holder;
 	public Button button;
+	public TextMeshProUGUI exitText;
+	bool pickedup;
+	bool dropped;
+	
+	void Update(){
+		if (pickedup){
+			if (Input.GetKeyDown(KeyCode.Escape)){
+				Drop();
+			}
+		}
+		if (dropped){
+			Destroy(this.gameObject);
+		}
+	}
 	
 	public void PickUp(){
 		Debug.Log("TOUCHED NOTE!");
@@ -15,9 +30,12 @@ public class Interactable : MonoBehaviour
 		button.interactable = false;
 		this.transform.position = holder.transform.position;
 		this.transform.parent = holder.transform;
+		exitText.enabled = true;
+		pickedup = true;
 	}
 	
 	public void Drop(){
-		
+		Debug.Log("NOTE DROPPED!");
+		dropped = true;
 	}
 }

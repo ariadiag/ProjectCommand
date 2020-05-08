87027be293f3bc9b1c364a6f1a389f3d8c84140f
ariadiag <ariadiag@users.noreply.github.com>
2020-05-08 13:55:23 -0400

    Fixed and finished

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -49,7 +49,7 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu ()
     {
-        SceneManager.LoadScene("StartMenu");
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame ()
